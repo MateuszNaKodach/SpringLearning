1fc7ad8494bada905156b8afc5834b782f0b995a
Mateusz Nowak <nmateusz96@gmail.com>
2017-04-08 18:52:03 +0200

    Add Spring Security

--- src/main/java/pl/nowakprojects/configurations/SecurityConfiguration.java
+++ src/main/java/pl/nowakprojects/configurations/SecurityConfiguration.java
@@ -1,17 +1,35 @@
 package pl.nowakprojects.configurations;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * Created by Mateusz on 08.04.2017.
  */
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private static final String ROLE_ADMIN = "pl.nowakprojects.spirngLearn.ADMIN";
+    private static final String ROLE_USER = "pl.nowakprojects.spirngLearn.USER";
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().permitAll();
+       // http.authorizeRequests().anyRequest().permitAll();
+        http.authorizeRequests().antMatchers("/start").permitAll();
+        http.authorizeRequests().antMatchers("/user").hasRole(ROLE_USER);
+        http.authorizeRequests().antMatchers("/admin").hasRole(ROLE_ADMIN);
+      //  http.httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password("admin").roles(ROLE_USER)
+                .and()
+                .withUser("user").password("user1").roles(ROLE_ADMIN);
     }
 }
---
+++ src/main/java/pl/nowakprojects/controllers/FrontendController.java
@@ -0,0 +1,30 @@
+package pl.nowakprojects.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by Mateusz on 08.04.2017.
+ */
+@Controller
+public class FrontendController {
+
+    @RequestMapping("/start")
+        String start(){
+            return "index";
+        }
+
+
+    @RequestMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+
+    @RequestMapping("/user")
+    public String user() {
+        return "user";
+    }
+}
